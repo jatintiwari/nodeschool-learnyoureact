604ebca1971513966e5a23300d9bd04012758d83
jatin.tiwari <jatin.tiwari@Jatin.local>
2018-05-06 17:43:08 +0530

    smaller views

---
+++ views/todoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TodoForm extends React.Component {
+  render() {
+    return (
+      <div className="todoForm">
+        I am a TodoForm.
+			</div>
+    )
+  }
+}
